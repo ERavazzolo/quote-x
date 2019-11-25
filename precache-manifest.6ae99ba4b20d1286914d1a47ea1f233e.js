e66ef564bb24d8e83bcbeee1efe98da93ee66857
ERavazzolo <erika.ravazzolo15@gmail.com>
2019-11-25 11:34:33 +0000

    Updates

---
+++ precache-manifest.6ae99ba4b20d1286914d1a47ea1f233e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4bbe9145bf6cf217723b8e5f24ea7b",
+    "url": "/quote-x/index.html"
+  },
+  {
+    "revision": "d4c0bed84ce946883741",
+    "url": "/quote-x/static/css/main.8f0fabf3.chunk.css"
+  },
+  {
+    "revision": "c13dcfb625ef9be3ad6e",
+    "url": "/quote-x/static/js/2.d5f5d618.chunk.js"
+  },
+  {
+    "revision": "d4c0bed84ce946883741",
+    "url": "/quote-x/static/js/main.ea73b1c5.chunk.js"
+  },
+  {
+    "revision": "4a4d21f139f64dd32ad7",
+    "url": "/quote-x/static/js/runtime-main.a59ce8bb.js"
+  },
+  {
+    "revision": "45823dec5a608cca99c1ae2b3f77b41f",
+    "url": "/quote-x/static/media/background-small.45823dec.jpg"
+  },
+  {
+    "revision": "716c13bd0113889ef82a0daf387d67a6",
+    "url": "/quote-x/static/media/spinner.716c13bd.gif"
+  }
+]);
